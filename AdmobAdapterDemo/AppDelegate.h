744edea9d09ef8c418a2d2b11f1fb9fa0839f66e
zengyang <zywork@bytedance.com>
2021-08-02 11:18:26 +0800

    Initial Commit

---
+++ AdmobAdapterDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AdmobAdapterDemo
+//
+//  Created by Eason on 2021/8/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AdmobAdapterDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AdmobAdapterDemo
+//
+//  Created by Eason on 2021/8/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
